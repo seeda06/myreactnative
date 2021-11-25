b83dbd14a0e55ef30f4845bd2abfa240f9e39543
seeda06 <86487372+seeda06@users.noreply.github.com>
2021-11-25 18:00:31 +0700

    week2

---
+++ screens/week2/Ex11.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Ex11() {
+  return <View style={{ flex : 1 , flexDirection:'column',alignItems:'stretch'
+}}>
+<View style={{height: 500, backgroundColor: '#4A90E2'}} />
+<View style={{height: 500, backgroundColor: '#50E3C2'}} />
+</View>
+}
---
+++ screens/week2/Ex7.js
@@ -0,0 +1,38 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Ex7() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        alignItems: "flex-end",
+      }}
+    >
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "#50E3C2",
+        }}
+      />
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "#4A90E2",
+        }}
+      />
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "#9013FE",
+        }}
+      />
+    </View>
+  );
+}
