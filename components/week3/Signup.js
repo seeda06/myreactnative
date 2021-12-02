3f792bc76d6fb1fe2a15893f6818051a4a9d0968
seeda06 <86487372+seeda06@users.noreply.github.com>
2021-12-02 17:35:18 +0700

    week3

---
+++ components/week3/Signup.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { ScrollView, StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function Signup() {
+    return (        
+        <View style={{ flex: 1, flexDirection: 'column' , justifyContent : 'center' , alignItems : 'stretch' }}>
+            <TextInput placeholder="Input ID"/>
+            <TextInput placeholder="Input E-mail"/>
+            <TextInput placeholder="Input Address"/>
+            <Button title="Sign up" color="red"/>
+        </View>
+    );
+}
